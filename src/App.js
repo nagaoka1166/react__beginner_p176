5feeaadc51ebd1b4c5a2c426df895586147bd6b5
Ryunosuke Nagoka <69971830+nagaoka1166@users.noreply.github.com>
2021-01-04 01:07:38 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,82 @@
+
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import Rect from './Rect';
 import './App.css';
-import Memo from './memo/Memo';
-import AddForm from './memo/Addform';
-import Findform from './memo/FindForm';
-import DelForm from './memo/DelForm';
 
-class app extends Component {
-    td = {
-        width:"25opx"
+
+class App extends Component {
+  input = '';
+
+
+  msgStyle = {
+    fontSize:"20pt",
+    color:"#900",
+    margin:"20px 0px",
+    padding: "5px",
+  }
+
+
+  constructor(props){
+    super(props);
+    this.state = {
+      message:'type your name:'
+    };
+    this.doCheck = this.doCheck.bind(this);
+  }
+
+
+  doCheck(event) {
+    alert(event.target.value +
+      "は長すぎます。(最大10文字)");
+  }
+
+
+  render(){
+    return <div>
+      <h1>React</h1>
+      <h2>{this.state.message}</h2>
+      <Message maxlength="10" onCheck={this.doCheck} />
+    </div>;
+  }
+    render(){
+    return <div>
+        <h1><React</h1>
+        <h2>{this.state.message} </h2>
+        <Message maxlength="10" onCheck={this.doCheck} />
+    </div>;
     }
-    constructor(props){
-        
-        super(props);
-    }
-    
-    render() {
-        return (
-           <div>
-            <h1>Memo</h1>
-            <Addform />
-            <hr />
-            <table><tbody><tr>
-            <td style={this.td}><Findform /></td>
-            <td style={this.td}><Delform /></td>
-            </tr></tbody></table>
-           </div>
- 
-        )
-        
-    }
+
+
 }
-    
-       
-export default connect() (App);
 
 
-                        
+class Message extends Component {
+  inputStyle = {
+    fontSize:"12pt",
+    padding:"5px"
+  }
+
+
+  constructor(props){
+    super(props);
+    this.doChange = this.doChange.bind(this);
+  }
+
+
+  doChange(e){
+    if (e.target.value.length > this.props.maxlength){
+      this.props.onCheck(e);
+      e.target.value =
+        e.target.value.substr(0,this.props.maxlength);
+    }
+  }
+
+
+  render(){
+    return <input type="text" style={this.inputStyle}
+      onChange={this.doChange} />
+  }
+}
+
+
+export default App;
